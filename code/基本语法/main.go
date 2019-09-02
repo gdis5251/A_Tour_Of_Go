5c2cbfe48807570f47e9cf7481ec9a15f259edc2
gdis5251 <841784596@qq.com>
2019-09-02 15:28:44 +0800

    [Gerald][write][2019/9/2] 循环和条件分支语句 加 defer 关键字的学习

--- code/基本语法/main.go
+++ code/基本语法/main.go
@@ -43,6 +43,44 @@ func main() {
 	// strL, strR := Swap("Make", "Money")
 	// fmt.Println(strL, strR)
 
-	fmt.Println(split(17))
+	// fmt.Println(split(17))
 
+	// 循环
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println("I will join in ByteDance be a bytedancer.", i)
+	// }
+
+	// 使用 for 来替代 C 中的 while
+	// count := 10
+	// for count >= 0 {
+	// 	fmt.Println(count)
+	// 	count--;
+	// }
+
+	// 无限循环
+	// for {
+	// 	 不需要写条件，只写一个 for 是无限循环
+	// }
+
+	// if-else 语句
+	// ByteDancer := true 
+	// if ByteDancer {
+	// 	fmt.Println("congratulation!")
+	// } else {
+	// 	fmt.Println("It is alright, TW still pretty good! And congratulation!")
+	// }
+
+	// defer 关键字
+	// 推迟调用的函数其参数会立即求值，但直到外层函数返回前该函数都不会被调用。
+	// defer fmt.Println("Go, believe yourself!")
+	// fmt.Println("I'd like to join ByteDance!")
+
+	// defer 栈
+	fmt.Println("counting:")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done!")
 }
