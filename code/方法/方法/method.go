24c1517fc83e48b7b28f3f2a126be555d18aa0b1
gdis5251 <841784596@qq.com>
2019-09-10 16:59:49 +0800

    [Gerald][write][2019/9/10] Go 语言基础

---
+++ code/方法/方法/method.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	x, y float64 
+}
+
+// func (v Vertex) Abs() float64 {
+// 	return math.Sqrt(v.x * v.x + v.y * v.y)
+// }
+
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.x * v.x + v.y * v.y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	// fmt.Println(v.Abs())
+	fmt.Println(Abs(v))
+}
