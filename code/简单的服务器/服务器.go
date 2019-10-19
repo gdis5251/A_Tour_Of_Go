993779315848803199e45b74f96660a3e047e88c
gdis5251 <841784596@qq.com>
2019-10-19 15:33:58 +0800

    [Gerald][write][2019/10/19] 完成了一个简单的回显服务器，客户端使用 netcat 来扮演测试。

---
+++ code/简单的服务器/服务器.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+// 简单的回显服务器
+func main() {
+	// 1. 监听
+	listener, listenErr := net.Listen("tcp", ":9090")
+	if listenErr != nil {
+		fmt.Println("listen error = ", listenErr)
+		return
+	}
+	defer listener.Close()
+
+	// 2. 阻塞等待
+	for true {
+		conn, acceptErr := listener.Accept()
+		if acceptErr != nil {
+			fmt.Println("accpet error = ", acceptErr)
+			continue
+		}
+		defer conn.Close()
+
+		// 3. 接收客户发来的数据
+		for true {
+			buffer := make([]byte, 1024)
+			n, readErr := conn.Read(buffer)
+			if readErr != nil {
+				fmt.Println("read error = ", readErr)
+			}
+			if n == 0 {
+				fmt.Println("client out")
+				break
+			}
+			fmt.Println("client said : ", string(buffer[:n]))
+		}
+	}
+}
